474b5ccaf16045944a9006adf5d60c9ce22faadc
Daryl <139952348+doughryl@users.noreply.github.com>
2023-10-02 20:24:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const addToCartButton1 = document.getElementById("addToCartButton1");
+  const addToCartButton2 = document.getElementById("addToCartButton2");
+  const addToCartButton3 = document.getElementById("addToCartButton3");
+
+  addToCartButton1.addEventListener("click", function() {
+    alert("Added to Cart");
+  });
+
+  addToCartButton2.addEventListener("click", function() {
+    alert("Added to Cart");
+  });
+
+  addToCartButton3.addEventListener("click", function() {
+    alert("Added to Cart");
+  });
+});
